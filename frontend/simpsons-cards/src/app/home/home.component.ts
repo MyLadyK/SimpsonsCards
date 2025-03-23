b13d61f9b9d53101efffacf6f13db7e7a61fda46
KuroKami <grisella.pd@gmail.com>
2025-03-23 18:02:28 +0000

    Home buttons done

--- frontend/simpsons-cards/src/app/home/home.component.ts
+++ frontend/simpsons-cards/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,4 +9,14 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   standalone: true
 })
-export class HomeComponent { }
+export class HomeComponent {
+  constructor(private router: Router) {}
+
+  goToSignIn() {
+    this.router.navigate(['/sign-in']);
+  }
+
+  goToSignUp() {
+    this.router.navigate(['/sign-up']);
+  }
+}
